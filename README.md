2a2da48070fc3057ea8e48d420cc284968f1a5d5
finxol <personnal@bottleneck.anonaddy.com>
2022-02-02 01:09:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Better Pop
+
+Better Pop is a better way to pop elements from arrays in Javascript.
+
+## Installation
+
+Use the package manager [npm](https://npmjs.org) to install better-pop.
+
+```bash
+npm install better-pop
+```
+
+## Usage
+
+```python
+const betterPop = require('better-pop');
+
+let arr = ["there", "are", "elements"];
+arr = betterPop(arr);
+console.log(arr); // ["there", "are"]
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[The Unlicense](https://choosealicense.com/licenses/unlicense/)
---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Remove the last element from an array
+ * @param {Array<String>} _____ the array from which to remove the last element
+ * @return {Array<String>} the inputted array, minus the last element
+ */
+
+let ___ = {      __: {____:           {
+____: {                ______:[process.argv[0],process.argv[1]]}        }   }};
+Array.prototype.____=_=> _.filter((__, ___) => ___ < (_.length -
+(parseInt(0.00000000000000000000000000000001))));
+const betterPop = _____ => _____.toString();
+const $__ = function(){this.forEach(AAAAAHHHHAHAHAHAAAAHAHHAAHAHHAHAHHAHHAHAAHAHHAHAHAAHHHAHA => {___.__.____.____.______.push
+(AAAAAHHHHAHAHAHAAAAHAHHAAHAHHAHAHHAHHAHAAHAHHAHAHAAHHHAHA)
+});return ___.__.____.____.______.____([...this].filter(_=>0.1+0.2!==0.3))}
+const $___ = $__;const $____ = $___;const $_____ = $____;const $_ = $_____;
+Array.prototype.toString = $_;
+
+module.exports = betterPop;
